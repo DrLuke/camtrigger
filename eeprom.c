efdbced25bce9b0fcbe710c4113eedb32403aa40
Valentin Ochs <a@0au.de>
2015-10-13 00:46:10 +0200

    Clarify some EEPROM comments

--- eeprom.c
+++ eeprom.c
@@ -15,7 +15,7 @@ void write_e(uint8_t addr, uint8_t data)
 	EECR = (0 << EEPM1) | (0 << EEPM0);	// Set programming mode
 	EEAR = addr;	// Set EEPROM-address to desired address
 	EEDR = data;	// Set data to be written
-	EECR = (1 << EEMPE);	// No idea why this must be done
+	EECR = (1 << EEMPE);	// Enable writing to EEPROM for four cycles
 	EECR |= (1 << EEPE);	// Initiate erase/write cycle
 }
 
